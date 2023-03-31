56075f6d3afb90207b8e27a16d94dbcabdb7fce7
Thomas Adika <115192563+burnermkali@users.noreply.github.com>
2023-03-31 09:06:03 +0300

    drools.py

---
+++ drools.py
@@ -0,0 +1,8 @@
+from pyjackson import deserialize, serialize
+from jsonpickle import decode, encode
+
+rules = '''
+import com.sample.Client;
+
+global java.util.List kindly_do_not_consider;
+global java.util.List considerable
