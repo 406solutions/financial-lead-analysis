c62807f6837709656fb990362110f34e945f3324
Thomas Adika <115192563+burnermkali@users.noreply.github.com>
2023-03-31 08:46:54 +0300

    Create install_libraries.sh

---
+++ install_libraries.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update package list and install pip
+sudo apt-get update
+sudo apt-get install python3-pip -y
+
+# Install pandas and Flask
+pip3 install pandas
+pip3 install Flask
+
+# Install pykafka
+pip3 install pykafka
+
+# Install Drools
+wget https://download.jboss.org/drools/release/7.54.0.Final/drools-distribution-7.54.0.Final.zip
+unzip drools-distribution-7.54.0.Final.zip
+rm drools-distribution-7.54.0.Final.zip
