7f1eb53a0c9a6b2adefd556f2ed8ca33b9b7c89c
Thomas Adika <115192563+burnermkali@users.noreply.github.com>
2023-04-02 10:38:26 +0300

    update

--- drools.py
+++ drools.py
@@ -1,8 +1,20 @@
-from pyjackson import deserialize, serialize
-from jsonpickle import decode, encode
+from drools.ruleunit import RuleUnit
+from drools.ruleunit import ruleunit
+from drools import Client
 
-rules = '''
-import com.sample.Client;
+@ruleunit(RuleUnit)
+class ClientRules:
+    def __init__(self, clients):
+        self.clients = clients
 
-global java.util.List kindly_do_not_consider;
-global java.util.List considerable
+    def execute(self, results):
+        for client in self.clients:
+            if client.age >= 18 and client.income >= 50000:
+                client.is_eligible = True
+                client.reason = 'Age and income meet eligibility criteria'
+            elif client.age >= 18 and client.income < 50000:
+                client.is_eligible = False
+                client.reason = 'Income does not meet eligibility criteria'
+            elif client.age < 18 and client.income >= 50000:
+                client.is_eligible = False
+                client.reason =
