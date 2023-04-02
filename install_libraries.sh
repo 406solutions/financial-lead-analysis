c9e3411573e2b976ced79d37e02efdc1ba1e9ea0
Thomas Adika <115192563+burnermkali@users.noreply.github.com>
2023-04-02 13:37:41 +0300

    Update install_libraries.sh

--- install_libraries.sh
+++ install_libraries.sh
@@ -11,6 +11,9 @@ pip3 install Flask
 # Install pykafka
 pip3 install pykafka
 
+#install csv 
+pip install python-csv
+
 # Install Drools
 wget https://download.jboss.org/drools/release/7.54.0.Final/drools-distribution-7.54.0.Final.zip
 unzip drools-distribution-7.54.0.Final.zip
